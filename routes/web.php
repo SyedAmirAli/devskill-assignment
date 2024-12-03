697969cb962586689ed7252cfa8e76c2cc7b4e25
SyedAmirAli <sywmohammadamir@gmail.com>
2024-12-03 23:13:56 +0600

    complete assignment

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guests')->group(function () {
     Route::group(['prefix' => '/auth', 'as' => 'auth.'], function () {
         Route::post('/login', [AuthController::class, 'login'])->name('login');
-        Route::post('/register', [AuthController::class, 'register'])->name('register');
     });
     
     Route::get('/', [DashboardController::class, 'home'])->name('home');
@@ -15,5 +14,6 @@ Route::middleware('guests')->group(function () {
 
 Route::group(['middleware' => ['authenticate']], function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
